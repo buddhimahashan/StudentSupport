1ca9801a9535479a56d16575da72fbd7e7149e68
buddhimahashan <buddhimahashan94@gmail.com>
2017-04-28 15:03:02 +0530

    Arosha_Final

--- src/pages/edit-public-notification/edit-public-notification.ts
+++ src/pages/edit-public-notification/edit-public-notification.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
 import { FirebaseListObservable, AngularFire } from 'angularfire2'
 import { AlertController } from 'ionic-angular';
 
